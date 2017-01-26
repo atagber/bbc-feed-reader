08d4a8edf12d759dafc27dff2dfead620e60178c
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-26 20:59:53 +0300

    <feature/news-posts-screen> Add blank NewsPostsListViewController and implement view for it

---
+++ bbc-feed-reader/NewsPostsListViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class NewsPostsListViewController: UITableViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  
+  }
+  
+  override func bindViewModel() {
+    super.bindViewModel()
+  }
+}
