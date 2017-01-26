b7a88c72cb78e9b746dc941f3d1e6bc5e8e5e446
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-26 23:28:34 +0300

    <feature/news-post-screen> Add helper for String to validate strings

---
+++ bbc-feed-reader/String+Validation.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+extension String {
+  static func isEmptyAfterTrimming(_ string: String?) -> Bool {
+    guard let string = String.trim(string) else {
+      return true
+    }
+    return string.isEmpty
+  }
+  
+  static func trim(_ string: String?) -> String? {
+    guard let string = string else {
+      return nil
+    }
+    return string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+  }
+}
