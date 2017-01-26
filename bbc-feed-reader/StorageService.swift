bf477cf46ec741e9da8b990edbb880bd07734ae3
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-26 16:22:21 +0300

    <feature/storage> Add method for saving and fetching NewsPosts to StorageService

--- bbc-feed-reader/StorageService.swift
+++ bbc-feed-reader/StorageService.swift
@@ -22,4 +22,37 @@ public class StorageService {
     let managedObjects = NewsPostMO.mr_findAll() as? [NewsPostMO] ?? []
     self.newsPostsProperty.value = managedObjects.flatMap(NewsPost.build(from:))
   }
+  
+  public func fetchNewsPosts(forCategory category: NewsCategory) -> SignalProducer<(success: Bool, error: BBCError?), NoError> {
+    return AppEnvironment.current.apiService.fetchNewsPosts(forCategory: category).materialize()
+      .filter { $0.value != nil || $0.error != nil }.map { [weak self] event in
+        if let error = event.error {
+          return (false, error)
+        }
+        if let value = event.value {
+          self?.syncNewsPostsWithDatabase(with: value)
+        }
+        return (true, nil)
+    }
+  }
+  
+  public func save(newsPost: NewsPost) {
+    self.syncNewsPostsWithDatabase(with: [newsPost])
+  }
+  
+  private func syncNewsPostsWithDatabase(with newsPosts: [NewsPost]) {
+    MagicalRecord.save({ context in
+      for newsPost in newsPosts {
+        if let managedObject = NewsPostMO.mr_findFirst(with: NSPredicate(format: "identifier = %@", newsPost.identifier), in: context) {
+          if newsPost.isChanged || !managedObject.isChanged {
+            newsPost.sync(managedObject: managedObject)
+          }
+        } else if let managedObject = NewsPostMO.mr_createEntity(in: context) {
+          newsPost.sync(managedObject: managedObject)
+        }
+      }
+    }) {[weak self] (_, _) in
+      self?.syncNewsPostsProperty()
+    }
+  }
 }
