1f43ca40297481dead0be67439e9cd37547d7db0
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-26 14:02:57 +0300

    <feature/root-tab-bar-view-controller> Add RootViewModel

--- bbc-feed-reader/RootTabBarViewController.swift
+++ bbc-feed-reader/RootTabBarViewController.swift
@@ -4,10 +4,18 @@ import Result
 import ReactiveSwift
 
 class RootTabBarViewController: UITabBarController {
+  let viewModel: RootViewModelType = RootViewModel()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    self.bindViewModel()
 
+    self.viewModel.inputs.viewDidLoad();
   }
-
+  
+  func bindViewModel() {
+    self.viewModel.outputs.setViewControllers.observe(on: UIScheduler())
+      .observeValues { [weak self] in self?.viewControllers = $0 }
+  }
 }
