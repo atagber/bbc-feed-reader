7946c81a04a8a71da691d6036ebb903e7ec0af3b
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-26 15:22:24 +0300

    <feature/api> Implement preparing request

--- bbc-feed-reader/ApiService.swift
+++ bbc-feed-reader/ApiService.swift
@@ -14,4 +14,52 @@ public class ApiService {
     config.requestCachePolicy = .reloadIgnoringLocalCacheData
     self.session = URLSession(configuration: config)
   }
+  
+  /// This params will be passed with every request to API (e.g. auth token)
+  private var defaultParams: [String:String] {
+    return [:]
+  }
+  
+  /// This headers will be passed with every request to API (e.g. language, client version)
+  private var defaultHeaders: [String:String] {
+    return [:]
+  }
+  
+  private func preparedRequest(_ route: Route) -> URLRequest {
+    let properties = route.requestProperties
+    
+    let url = self.serverConfig.apiBaseUrl.appendingPathComponent(properties.path)
+    let request = preparedRequest(url: url, method: properties.method, params: properties.params)
+    
+    return request
+  }
+  
+  private func preparedRequest(url: URL, method: Method = .GET, params: [String:Any] = [:]) -> URLRequest {
+    var request = URLRequest(url: url)
+    request.httpMethod = method.rawValue
+    request = self.preparedRequest(request: request, params: params)
+    return request
+  }
+  
+  private func preparedRequest(request originalRequest: URLRequest, params originalParams: [String:Any] = [:]) -> URLRequest {
+    var request = originalRequest
+    
+    guard let URL = request.url else {
+      return originalRequest
+    }
+    
+    var components = URLComponents(url: URL, resolvingAgainstBaseURL: false)!
+    var params = components.queryItems ?? []
+    params.append(contentsOf: self.defaultParams.map {URLQueryItem(name:$0, value:$1)})
+    params.append(contentsOf: originalParams.map {URLQueryItem(name:$0, value:String(describing: $1))})
+    components.queryItems = params
+    
+    request.url = components.url
+    
+    var headers = request.allHTTPHeaderFields ?? [:]
+    self.defaultHeaders.forEach { headers[$0] = $1 }
+    request.allHTTPHeaderFields = headers
+    
+    return request
+  }
 }
