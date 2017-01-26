64ede9b34c2498eeea530b36b6b3bba3c169d4b7
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-26 22:34:21 +0300

    <feature/status-screen> Connect ViewModel to StatusViewController

--- bbc-feed-reader/StatusViewController.swift
+++ bbc-feed-reader/StatusViewController.swift
@@ -3,22 +3,27 @@ import ReactiveCocoa
 import ReactiveSwift
 
 class StatusViewController: UIViewController {
+  fileprivate let viewModel: StatusViewModelType = StatusViewModel()
   @IBOutlet weak var statusImageView: UIImageView!
   @IBOutlet weak var statusMessageLabel: UILabel!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
+    self.viewModel.inputs.viewDidLoad()
   }
   
   override func bindViewModel() {
     super.bindViewModel()
-
+    
+    self.statusImageView.reactive.image <~ self.viewModel.outputs.statusImage
+    self.statusMessageLabel.reactive.text <~ self.viewModel.outputs.statusMessage
+    self.view.reactive.isHidden <~ self.viewModel.outputs.statusViewHidden
   }
 }
 
 extension StatusViewController {
   func configureWith(status: Status) {
-    // TODO: implement this method
+    self.viewModel.inputs.configureWith(status: status)
   }
 }
