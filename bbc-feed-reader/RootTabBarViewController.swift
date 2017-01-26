134a5b7babeeb9f27146aaea67262afe6fe24cab
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-26 13:50:59 +0300

    <feature/root-tab-bar-view-controller> Add blank RootTabBarViewController

---
+++ bbc-feed-reader/RootTabBarViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+import ReactiveCocoa
+import Result
+import ReactiveSwift
+
+class RootTabBarViewController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+
+}
