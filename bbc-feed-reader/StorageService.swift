d11e220c111623074b364be42bfa6524d78ac11f
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-26 16:18:49 +0300

    <feature/storage> Add MutableProperty for NewsPosts to StorageService and sync method

--- bbc-feed-reader/StorageService.swift
+++ bbc-feed-reader/StorageService.swift
@@ -5,6 +5,21 @@ import ReactiveSwift
 import Result
 
 public class StorageService {
+  private let newsPostsProperty = MutableProperty<[NewsPost]>([])
+  
+  public let newsPosts: Signal<[NewsPost], NoError>
+  
   init() {
+    self.newsPosts = newsPostsProperty.signal
+    self.syncAllProperties()
+  }
+  
+  private func syncAllProperties() {
+    self.syncNewsPostsProperty()
+  }
+  
+  private func syncNewsPostsProperty() {
+    let managedObjects = NewsPostMO.mr_findAll() as? [NewsPostMO] ?? []
+    self.newsPostsProperty.value = managedObjects.flatMap(NewsPost.build(from:))
   }
 }
