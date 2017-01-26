4ea511844a04e80aab0c93dcd872a67d27377553
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-26 18:58:14 +0300

    <develop> Add Images enum

---
+++ bbc-feed-reader/Images.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+enum Images: String {
+  
+  case defaultPlaceholder = "default-placeholder"
+  case bbcLogoTabBarIcon = "tabbar-bbc-icon"
+  case favoriteIcon = "star-icon"
+  case favoriteSelectedIcon = "star-selected-icon"
+  case warningIcon = "warning-icon"
+  case waitIcon = "wait-icon"
+  case sadFaceIcon = "sad-face-icon"
+  case customNewsImageURL = "http://www.bbc.co.uk/news/special/2015/newsspec_10857/bbc_news_logo.png"
+  
+  var image: UIImage {
+    return UIImage(named: self.rawValue)!
+  }
+}
