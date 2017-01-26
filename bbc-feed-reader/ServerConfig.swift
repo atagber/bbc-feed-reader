1883972d2eba8605af79edec6b640b57665030c4
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-26 14:48:50 +0300

    <feature/api> Add ServerConfig struct

---
+++ bbc-feed-reader/ServerConfig.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+public struct ServerConfig {
+  public let apiBaseUrl: URL
+  
+  public static let production: ServerConfig = ServerConfig(
+    // TODO: move link to secrets
+    apiBaseUrl: URL(string: "http://feeds.bbci.co.uk/")!
+  )
+  
+  public static let staging: ServerConfig = ServerConfig(
+    // TODO: use staging base url
+    // TODO: move link to secrets
+    apiBaseUrl: URL(string: "http://feeds.bbci.co.uk/")!
+  )
+}
