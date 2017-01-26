515f54659cd53eee8663ac800a65d35b5325a3d4
Arman Tagbergenev <arman.tagbergenev@studiomobile.ru>
2017-01-27 00:31:22 +0300

    <develop> Add secrets enum

---
+++ bbc-feed-reader/Secrets.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+enum Secrets: String {
+  case productionServerBaseURL = "http://feeds.bbci.co.uk/"
+  case stagingServerBaseURL = "http://staging.bbci.co.uk/"
+}
--- bbc-feed-reader/ServerConfig.swift
+++ bbc-feed-reader/ServerConfig.swift
@@ -4,13 +4,10 @@ public struct ServerConfig {
   public let apiBaseUrl: URL
 
   public static let production: ServerConfig = ServerConfig(
-    // TODO: move link to secrets
-    apiBaseUrl: URL(string: "http://feeds.bbci.co.uk/")!
+    apiBaseUrl: URL(string: Secrets.productionServerBaseURL.rawValue)!
   )
 
   public static let staging: ServerConfig = ServerConfig(
-    // TODO: use staging base url
-    // TODO: move link to secrets
-    apiBaseUrl: URL(string: "http://feeds.bbci.co.uk/")!
+    apiBaseUrl: URL(string: Secrets.stagingServerBaseURL.rawValue)!
   )
 }
